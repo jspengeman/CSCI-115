801fcb5142f9eed3aba664caabcae5713c5efcca
Jonathan <jonathan.spengeman@gmail.com>
2015-02-07 17:22:21 -0800

    Tower of Hanoi problem completed for 1 to 2N rings

--- Lab 2/hanoi.cpp
+++ Lab 2/hanoi.cpp
@@ -11,38 +11,46 @@ HanoiTower::HanoiTower(int n){
 	}
 
 	// Intializes second ring from n+1 to 2n
-	// for (int s = n + 1; s <= 2 * n; s++){
-	// 	rings[1].push_back(s);
-	// }
+	for (int s = n + 1; s <= 2 * n; s++){
+		rings[1].push_back(s);
+	}
 }
 
 void HanoiTower::display(){
 	for (int i = 0; i <= 2; i++){
 		int size = rings[i].size();
 		cout << "Tower " << i << ": ";
-		if (rings[i].empty() != true){
-			for (int j = 0; j < size; j++){
-				cout << rings[i][j] << " ";
-			}
+		
+		for (int j = 0; j < size; j++){
+			cout << rings[i][j] << " ";
 		}
 		cout << endl;
 	}
 }
 
 void HanoiTower::move(int start, int goal){
-	// Takes the top ring from pole start and moves it to pole goal
+	// Takes the top ring from tower start and moves it to tower goal
+	cout << "(" << start << ", " << goal << ")" << endl;
 	int temp_ring = rings[start][0];
 	rings[start].erase(rings[start].begin());
 	rings[goal].insert(rings[goal].begin(), temp_ring);
 }
 
-void HanoiTower::solve(int n, int start, int goal, int temp){
+void HanoiTower::solve_prime(int n, int start, int goal, int temp){
+	// Adapted the solution from the textbook to fit my needs
 	if (n == 0) return; 
-	solve(n - 1, start, temp, goal);
+	solve_prime(n - 1, start, temp, goal);
 	move(start, goal);
-	solve(n - 1, temp, goal, start);
+	solve_prime(n - 1, temp, goal, start);
+}
 
-	// solve(n - 1, start + 1, goal, temp - 1);
-	// move(start, goal);
-	// solve(n - 1, temp - 1, goal, start + 1);
+void HanoiTower::solve(int n, int start, int goal, int temp){
+	/* Since tower 2 would contain N + 1 to 2N rings we also 
+	 * need to move those as well. Those should all be moved
+	 * first since they are going to be larger. Since they are
+	 * larger it is really like two sub problems. First solve 
+	 * N + 1 to 2N then you will be able to solve 1 to N.
+	 */
+	solve_prime(n, start + 1, goal, temp - 1);
+	solve_prime(n, start, goal, temp);
 }
--- Lab 2/hanoi.h
+++ Lab 2/hanoi.h
@@ -7,6 +7,7 @@ class HanoiTower {
 		void display();
 		void move(int start, int goal);
 		void solve(int n, int start, int goal, int temp);
+		void solve_prime(int n, int start, int goal, int temp);
 	private:
 		std::array<std::vector<int>, 3> rings;
 };
