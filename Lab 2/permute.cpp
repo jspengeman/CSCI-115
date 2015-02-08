c8e42212f4c859c5b1af24819d4c1cddb789c07e
Jonathan <jonathan.spengeman@gmail.com>
2015-02-08 14:19:53 -0800

    Permutations algorithm completed

--- Lab 2/permute.cpp
+++ Lab 2/permute.cpp
@@ -1,48 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 
 using namespace std;
-void permute(int n);
-void permute_prime(vector<int> list, int start);
+void swap(int &i, int &j);
+void permute(int n, int i);
+void std_permute(vector<int> list);
 
-int main(){
-	int n;
-	cout << "Enter an integer: ";
-	cin >> n;
-	permute(n);
+int *nums;
+
+int main() {
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+    nums = new int[n]; 
+    
+    // Intialize the set of numbers
+    for (int i = 0; i < n; i++) { nums[i] = i + 1; }
+
+    permute(n, 0);
+    return 0;
 }
 
-void permute(int n){
-	// Declare and intialize the set
-	vector<int> list;
-	for (int i = 0; i < n; i++){
-		list.push_back(i + 1);
-	}
-
-	permute_prime(list, 0);
+void std_permute(vector<int> list){
+    // Using std::algorithm to implement permutations
+    do {
+        for (int i = 0; i < list.size(); i++){
+            cout << list[i] << " ";
+        }
+        cout << endl;
+    } while (std::next_permutation(list.begin(), list.end()));
 }
 
-void permute_prime(vector<int> list, int start){
-	cout << "start: " << start << endl;
-	if (start == list.size()) return;
+void permute(int n, int i){
+    // Using my own implementation for permutations
+    if (i == n - 1) {
+        int j = 0;
+        // Display current permutation
+        while (j < n) {
+            cout << nums[j] << " ";
+            j++;
+        }    
+    cout << endl;
+    }
+    else {
+        int j = i;
+        // Generate permutation
+        while (j < n) {
+            swap(nums[i], nums[j]);
+            permute(n, i + 1);     
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
 
-	int i = start;
-	while (i < list.size()){
-		cout << list[i] << " ";
-		// display right elements of the set
-		for (int x = i + 1; x < list.size(); x++){
-			cout << list[x] << " ";
-		}
-		
-		// display left elements of the set
-		for (int y = i - 1; y >= 0; y--){
-			cout << list[y] << " ";
-		}
-		cout << endl;
-
-		i++;
-	}
-	
-	permute_prime(list, start + 1);  
-};
-
+void swap(int &i, int &j){
+    int t = i;
+    i = j;
+    j = t;
+}
