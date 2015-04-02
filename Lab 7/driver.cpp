4d26a39732fdbafa222827c823f70920aa9b5f7d
Jonathan <jonathan.spengeman@gmail.com>
2015-04-02 16:58:03 -0700

    Changed my driver file

--- Lab 7/driver.cpp
+++ Lab 7/driver.cpp
@@ -3,28 +3,20 @@
 using namespace std;
 
 int main(){
-	// int arr[20] = {50, 45, 40, 35, 30, 25, 20};
-	// heap<int, Min> *TestHeap = new heap<int, Min>(arr, 7, 20);
-	// TestHeap->viewHeap();
-	// cout << TestHeap->removefirst() << endl;
-	// cout << TestHeap->removefirst() << endl;
-	// cout << TestHeap->removefirst() << endl;
-	// cout << TestHeap->removefirst() << endl;
-	// cout << endl;
-	// TestHeap->viewHeap();
-	
-	
-	HuffTree<char>* test[8];
-    test[0] = new HuffTree<char> ('z', 2);
-    cout << test[1] << endl;
-    test[1] = new HuffTree<char> ('k', 30);
-    test[2] = new HuffTree<char> ('m', 24);
-    test[3] = new HuffTree<char> ('c', 32);
-    test[4] = new HuffTree<char> ('u', 37);
-    test[5] = new HuffTree<char> ('d', 42);
-    test[6] = new HuffTree<char> ('l', 42);
-    test[7] = new HuffTree<char> ('e', 120);
+	HuffTree<char>* huff_trees[10];
+    huff_trees[0] = new HuffTree<char> ('z', 10);
+    huff_trees[1] = new HuffTree<char> ('k', 30);
+    huff_trees[2] = new HuffTree<char> ('m', 24);
+    huff_trees[3] = new HuffTree<char> ('c', 32);
+    huff_trees[4] = new HuffTree<char> ('u', 45);
+    huff_trees[5] = new HuffTree<char> ('d', 23);
+    huff_trees[6] = new HuffTree<char> ('g', 12);
+    huff_trees[7] = new HuffTree<char> ('e', 128);
+    huff_trees[8] = new HuffTree<char> ('l', 12);
+    huff_trees[9] = new HuffTree<char> ('a', 120);
 
-    HuffTree<char>* final = buildHuff (test, 8);
+    cout << huff_trees[1] << endl;
+
+    HuffTree<char>* final = buildHuff (huff_trees, 8);
 	return 0;
 }
